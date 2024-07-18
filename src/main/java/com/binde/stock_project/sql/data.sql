ae6b34ee0011368f322fb0b74d45742286516a1b
NANNIM BINDE <59848588+binde318@users.noreply.github.com>
2024-07-18 23:44:30 +0100

    Update data.sql

--- src/main/java/com/binde/stock_project/sql/data.sql
+++ src/main/java/com/binde/stock_project/sql/data.sql
@@ -14,6 +14,7 @@ ORDER BY T1.id;
 
 #Explanation:
 #SELECT T1.*: This selects all columns from the T1 table.
-#LEFT JOIN T2 ON T1.id = T2.id: This performs a left join between T1 and T2 on the id column. The left join ensures that all records from T1 are included, and matching records from T2 are also included if they exist.
+#LEFT JOIN T2 ON T1.id = T2.id: This performs a left join between T1 and T2 on the id column. 
+#The left join ensures that all records from T1 are included, and matching records from T2 are also included if they exist.
 #WHERE T2.id IS NULL: This condition filters out the rows where there is a match in T2. Essentially, it keeps only the rows from T1 that do not have a corresponding match in T2.
 #ORDER BY T1.id: This orders the result set based on the id column of T1.
